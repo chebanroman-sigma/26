7b225928a0acda4c416f6a7308992699d3965556
chebanroman-sigma <r3388967@gmail.com>
2025-02-19 09:33:38 +0200

    Add files via upload

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+
+        public static int[] Input()
+        {
+            Console.Write("Введите количество элементов:" );
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите элемент массива [{0}]: ", i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+
+       
+        
+        public static void Output(int[] array)
+        {
+            Console.WriteLine("Вывод массива:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] omas= Input();
+            Output(omas);
+           
+            Console.ReadKey();
+        }
+    }
+}
