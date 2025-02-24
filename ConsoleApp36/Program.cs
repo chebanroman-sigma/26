259940459118160aef84d3f957e12f81999adadb
chebanroman-sigma <r3388967@gmail.com>
2025-02-24 17:50:20 +0200

    Add files via upload

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        
+
+        static int[] Metod_Random (int size, int min, int max)
+        {
+            int[] omas = new int[size];
+            Random random = new Random();
+
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = random.Next(-10,10);
+            }
+
+            return omas;
+        }
+
+
+        static void Output(int[] array)
+        {
+            Console.WriteLine("Сгенерированный массив:");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        static int Value(int[] array)
+        {
+            
+            return array.Count(x => x % 2 == 0);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] array = Metod_Random(10, -10, 10);
+            Output(array);
+            Console.WriteLine($"Количество четных элементов: {Value(array)}");
+
+            Console.ReadLine();
+        }
+    }
+}
